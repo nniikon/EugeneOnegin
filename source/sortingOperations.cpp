5b7890f9b63bccb223abb11b03f025780ec22fc9
nnikon <kolya.kasparov@gmail.com>
2023-09-19 22:11:47 +0300

    fix swap()
    size_t <- int

--- source/sortingOperations.cpp
+++ source/sortingOperations.cpp
@@ -140,26 +140,26 @@ static void swap(void* str1, void* str2, const size_t elemSize)
 {
     assert(str1 != NULL && str2 != NULL);
 
-    int currentSize = elemSize;
-    // printf("\t\tWHILE START\n");
-    while(currentSize >= int(sizeof(__int64)))
+    size_t currentSize = elemSize;
+    while(currentSize >= sizeof(__int64))
     {
         // printf("1current size = %d\n", currentSize);
         __int64 ltemp = *((__int64*)str1);
         *((__int64*)str1) = *((__int64*)str2);
         *((__int64*)str2) = ltemp;
         currentSize -= sizeof(__int64);
-        // printf("2current size = %d\n", currentSize);
+        
+        if (currentSize == 0)
+            return;
 
         str1 = (void*)((size_t)str1 + sizeof(__int64));
         str2 = (void*)((size_t)str2 + sizeof(__int64));
     } 
-
     
     char* char1 = (char*)str1;
     char* char2 = (char*)str2;
 
-    for (int j = 0; j < currentSize; j++)
+    for (size_t j = 0; j < currentSize; j++)
     {
         char ctemp = char1[j];
         char1[j] = char2[j];
